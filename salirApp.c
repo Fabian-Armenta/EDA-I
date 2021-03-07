72ea01b779104365b2f93c0233723430c551a413
Fabian-Armenta <72715671+Fabian-Armenta@users.noreply.github.com>
2021-03-07 13:10:50 -0600

    Delete salirApp.c

--- salirApp.c
+++
@@ -1,5 +0,0 @@
-void salirApp(){
-    system("cls");
-    printf("Saliendo de la aplicacion, un gusto atenderte\n");
-    printf("Recuerda que una flor dice mas que mil palabras :)\n Hasta luego!");
-}
