9218bc1290a6c1c4355f4e99bdda4ed4e4cffd88
Aleksei115 <70932379+Aleksei115@users.noreply.github.com>
2021-03-09 16:17:57 -0600

    linea de más

--- Práctica1_EDA1/funciones.c
+++ Práctica1_EDA1/funciones.c
@@ -1,5 +1,4 @@
 #define NUM_PRODUCT 5
-void menu();
 char productos [NUM_PRODUCT][15] = {"Rosas", "Girasoles", "Clavel", "Alcatraz", "Tulipanes"};
 int numeros [3][NUM_PRODUCT] = {{15, 30, 12, 35, 70}, //precio
                                  {100, 50, 40, 30,70}, //cantidad
